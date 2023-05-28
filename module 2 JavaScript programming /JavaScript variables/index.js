e29c94f2cbc719d88a7d86351c658175f9128cb2
Sergey Maldenberg <maldenberg@mail.ru>
2023-05-28 23:56:10 +0300

    JS 3

---
+++ module 2 JavaScript programming /JavaScript variables/index.js
@@ -0,0 +1,37 @@
+// Что такое переменная
+let a = 1;
+console.log(a);
+
+// Зачем нужна переменная
+
+let myName = "Сергей"
+console.log(myName);
+
+let myAge = 30;
+console.log(myAge);
+myAge = 32
+
+// Способы объявить переменную var, let, const
+
+let a = 3;
+var t = 4;
+const l = 5;
+
+// Константы
+
+const d = 7;
+
+// Как называть переменную
+
+let sergey;
+let myName; //Camel Case
+let my_name 
+
+// Почему важно давать понятные названия
+
+const weekDays = 7; // число, которое везде будет использоваться после заведения юзера
+const WEEK_DAYS = 9; // число, которое будет использоваться везде и оно известно изначально
+
+    
+}
+
