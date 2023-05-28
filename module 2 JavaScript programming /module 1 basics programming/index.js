8023dc6d25b001f651e0b68fcb4fc3ff7114d828
Sergey Maldenberg <maldenberg@mail.ru>
2023-05-28 23:26:45 +0300

    javascript homework1

---
+++ module 2 JavaScript programming /module 1 basics programming/index.js
@@ -0,0 +1,21 @@
+console.log("Проснуться в 9:00");
+console.log("Почистить зубы");
+
+const condition2 = true;
+
+if (condition2) {
+    console.log("Налить кофе");
+} else {
+    console.log("Пойти почистить зубы");
+}
+
+console.log("Включить компуктер");
+console.log("Разбудить Тоню");
+
+const tonya = false;
+
+if (tonya) {
+    console.log("Дать пендаля");
+} else {
+    console.log("Разбудить принудительно");
+}
