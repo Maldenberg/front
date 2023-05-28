6e9d03cb3e8ce0bbee15428db2469584224c903b
Sergey Maldenberg <maldenberg@mail.ru>
2023-05-29 00:07:50 +0300

    homework 3

---
+++ module 2 JavaScript programming /JavaScript variables/Homework/index.js
@@ -0,0 +1,19 @@
+// 1
+let myName = "Sergey";
+console.log(myName);
+alert(myName)
+
+// 2
+let countMoney;
+
+// 3
+let age = 17;
+
+if (age < 18) {
+    console.log("Вам меньше 18");
+} else {
+    console.log("Вам больше 18");
+}
+
+
+
